feb95a10724a15abbcb6dcf440b95c534636d490
EdbertChan <edbert.chan@yahoo.com>
2014-01-14 20:46:47 -0800

    Added basic rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DoTA2WebsiteProjectFork::Application.config.secret_key_base = 'e7bc916c54550dfe93a086056707b853b543a20b19fd7e62efb5e4651e42526b754d585762bd38eb343b4a1ae0a4d1b7d22f55e63181fb5cb9d1ae65e7c326fb'
